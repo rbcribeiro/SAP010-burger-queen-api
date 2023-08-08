617e2f631f4b08335109b146fb49d2cef660f675
rbcribeiro <rbcribeiro@gmail.com>
2023-08-08 14:15:43 -0300

    Feat: Implementação de criação do usuário

--- models/modelUser.js
+++ models/modelUser.js
@@ -2,29 +2,30 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    primaryKey: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
-User.associate = (models) => {
-  User.hasMany(models.Product, { foreignKey: 'userId' });
-};
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-return User;
+  User.associate = (models) => {
+    User.hasMany(models.Product, { foreignKey: 'userId' });
+  };
+
+  return User;
 };
