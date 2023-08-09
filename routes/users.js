6f140e0c4701c1926163809c84af38e9ef0034a1
rbcribeiro <rbcribeiro@gmail.com>
2023-08-09 11:45:02 -0300

    Feat: implementação da função listar um ID

--- routes/users.js
+++ routes/users.js
@@ -34,27 +34,38 @@ module.exports = (app, next) => {
   app.get('/users', requireAdmin, getUsers);
 
 
-  app.get('/users/:uid', requireAuth, (req, resp) => {
+  app.get('/users/:uid', requireAuth, async (req, res) => {
+    try {
+      //pega o parametro da URL requisição
+      const uid = req.params.uid;
+      //busca no banco de dados pelo parametro
+      const user = await User.findOne({ where: { id: uid } });
+  
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+  
+      res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+    }
   });
 
+  app.post('/users', requireAdmin, async (req, resp, next) => {
+    try {
+      const { email, password, role } = req.body;
+      if (!email || !password || !role) {
+        return resp.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+      }
 
-app.post('/users', requireAdmin, async (req, resp, next) => {
-  try {
-    const { email, password, role } = req.body;
-    if (!email || !password || !role) {
-      return resp.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+      const newUser = await User.create({ email, password, role });
+      return resp.status(201).json(newUser);
+    } catch (error) {
+      return next(error);
     }
+  });
 
-    const newUser = await User.create({ email, password, role });
-    return resp.status(201).json(newUser);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-
-
- 
   app.put('/users/:uid', requireAuth, (req, resp, next) => {
   });
 
