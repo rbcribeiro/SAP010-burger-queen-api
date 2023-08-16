c18bf337bfae86d2dc3b8c0dfbefe6aed95e82b3
rbcribeiro <rbcribeiro@gmail.com>
2023-08-16 14:22:34 -0300

    Teste: Inicio dos testes users

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,9 +1,98 @@
 const {
   getUsers,
-} = require('../users');
+} = require('../users'); // Certifique-se de usar o caminho correto para o módulo
+const models = require('../../models');
+
+jest.mock('../../models', () => ({
+  User: {
+    findAll: jest.fn(() => [
+      { 
+        id: '1',
+        email: 'ana@api.com',
+        password: '523',
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { 
+        id: '2',
+        email: 'rosana@api.com',
+        password: '987',
+        role: 'chef',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '3',
+        email: 'josé@api.com',
+        password: '456',
+        role: 'waiter',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]),
+  },
+}));
 
 describe('getUsers', () => {
-  it('should get users collection', (done) => {
-    done(getUsers);
+  it('Deve obter coleção de usuários - Teste 1', async () => {
+    const mockReq = {};
+    const mockResp = {
+      json: jest.fn(),
+    };
+    const mockNext = jest.fn();
+
+    await getUsers(mockReq, mockResp, mockNext);
+
+    expect(mockResp.json).toHaveBeenCalled();
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('Deve lidar com erro de banco de dados', async () => {
+    const mockReq = {};
+    const mockResp = {
+      json: jest.fn(),
+    };
+    const mockNext = jest.fn();
+    
+    // Simula um erro ao buscar dados do banco de dados
+    jest.spyOn(models.User, 'findAll').mockRejectedValue(new Error('Database error'));
+  
+    await getUsers(mockReq, mockResp, mockNext);
+  
+    // Verifica se a função next foi chamada com o erro esperado
+    expect(mockNext).toHaveBeenCalledWith(new Error('Database error'));
+  });
+
+  it('Deve lidar corretamente com chamadas assíncronas', async () => {
+    const mockReq = {};
+    const mockResp = {
+      json: jest.fn(),
+    };
+    const mockNext = jest.fn();
+  
+    // Simula um pequeno atraso para simular a chamada assíncrona
+    jest.spyOn(models.User, 'findAll').mockResolvedValue(new Promise(resolve => setTimeout(() => resolve([]), 100)));
+  
+    await getUsers(mockReq, mockResp, mockNext);
+  
+    // Verifica se a função json foi chamada após o atraso
+    expect(mockResp.json).toHaveBeenCalledWith([]);
+  });
+  
+  it('Deve lidar com coleção vazia de usuários', async () => {
+    const mockReq = {};
+    const mockResp = {
+      json: jest.fn(),
+    };
+    const mockNext = jest.fn();
+  
+    // Simula retorno de coleção vazia do banco de dados
+    jest.spyOn(models.User, 'findAll').mockResolvedValue([]);
+  
+    await getUsers(mockReq, mockResp, mockNext);
+  
+    // Verifica se a função json foi chamada com um array vazio
+    expect(mockResp.json).toHaveBeenCalledWith([]);
   });
 });
