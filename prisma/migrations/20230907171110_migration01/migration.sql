5c1cfb609b4b5c59f5feaf1735ea1160960bcef0
rbcribeiro <rbcribeiro@gmail.com>
2023-09-07 16:48:42 -0300

    prisma

--- prisma/migrations/20230907171110_migration01/migration.sql
+++ prisma/migrations/20230907171110_migration01/migration.sql
@@ -41,6 +41,8 @@ CREATE TABLE "products" (
     "price" DOUBLE PRECISION NOT NULL,
     "image" TEXT,
     "type" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
@@ -52,6 +54,8 @@ CREATE TABLE "users" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
